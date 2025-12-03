a0e5ddad4517008014a2eb548e0a187743bbf3a6
VyStewart <vyt.stewart@gmail.com>
2025-12-02 20:32:43 -0600

    Day 01 Python practice -print()
    Using commas, +, *

---
+++ daily-python/day01/practice.py
@@ -0,0 +1,15 @@
+# Day 1 - Basic print operation
+
+# Using comma, 
+print("Hello", "My name is","Vy") 
+print ("I am from", "Maestro AI University")
+
+# Using +
+print ("Learning Python is " + "fun!")
+print ( 10 + 3)
+
+# Using *
+print ("♥️" * 5)
+
+# Final Challegen
+print ("Wooo"* 3, "Final " + "challegen!", "Score:", 7 + 3, "⭐️" * 3)
