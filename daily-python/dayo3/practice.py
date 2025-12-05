00c45942359393d0e860810dec14d29f63bb6f25
VyStewart <vyt.stewart@gmail.com>
2025-12-04 19:14:38 -0600

    Day 03 Python practices - working with numbers and real-world calculations

---
+++ daily-python/dayo3/practice.py
@@ -0,0 +1,23 @@
+# Day 03 - Working with Numbers & Real-World Calculations
+
+# Game night cost (version 1)
+ticket_total = 7 * 5
+snack_total= 3 * 3
+print ("Total cost of the game night is:", ticket_total + snack_total)
+
+# Game night cost (version 2)
+
+# Total for tickets
+ticket_price = 7
+ticket_quantity = 5
+ticket_total = ticket_price * ticket_quantity
+
+# Total for snacks
+snack_price = 3
+snack_quantity = 3
+snack_total = snack_price * snack_quantity
+
+# Overal Total
+overall_total = ticket_total + snack_total
+print ("The total cost is:", overall_total)
+
