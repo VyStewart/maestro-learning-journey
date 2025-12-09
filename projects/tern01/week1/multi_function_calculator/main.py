b1e7147cb996aa4526961cd1cd0dbcdeb8fd914d
VyStewart <vyt.stewart@gmail.com>
2025-12-08 22:33:08 -0600

    Restructuring Maestro Repo and Update project Term 1 Week 1

---
+++ projects/tern01/week1/multi_function_calculator/main.py
@@ -0,0 +1,35 @@
+# Week 1 - Multi-Function Calculator
+# Author: Vy S
+# Description: Small tools for daily life (study time, cookies, etc.)
+
+# Description: Small tools for daily life (study time, cookies, etc.)
+"""
+print("----- Study Time Calculator -----")
+
+study_time = int(input("How many minutes do you spend on studying Python every day? "))
+full_hours = study_time // 60
+remain_minutes = study_time % 60
+
+hour_label = "hour" if full_hours == 1 else "hours"
+minute_label = "minute" if remain_minutes == 1 else "minutes"
+
+print(f"You study {full_hours} {hour_label} and {remain_minutes} {minute_label} every day.")
+
+"""
+# 2. Ask for cookies per day and estimate per month for a small bakery
+# They can sell cookies by individual or by box ( 9 per box)
+
+print("--------Daily Invetory Calculator-------")
+baked_cookies = int(input("How many cookies do you bake every day? "))
+sales_cookies = int(input("How many cookies do you sell per day? "))
+
+full_box = sales_cookies // 9
+remain_cookies = sales_cookies % 9
+leftover_cookies = baked_cookies - sales_cookies
+
+full_box_label = "box" if full_box == 1 else "boxes"
+remain_cookies_label = "cookie" if remain_cookies == 1 else "cookies"
+lefover_cookies_label = "cookie" if leftover_cookies == 1 else "cookies"
+
+print(f"You sell {full_box} {full_box_label} and {remain_cookies} {remain_cookies_label} every day."
+      f" And the leftover is {leftover_cookies} {lefover_cookies_label}.")
--- projects/tern01/week1/multi_function_calculator/practice.py
+++ projects/tern01/week1/multi_function_calculator/practice.py
@@ -18,8 +18,20 @@ remain_minutes = int(study_time) % 60
 
 print(f"You study {full_hours} hours and {remain_minutes} minutes a day.")
 
+# 2. Ask for cookies per day and estimate per month for a small bakery
+# They can sell cookies by individual or by box ( 9 per box)
+baked_cookies = int(input("How many cookies did you bake every day? "))
+sales_cookies = int(input("How many cookies do you sell every day? "))
+full_box = sales_cookies // 9
+remain_cookies = sales_cookies % 9
+left_over = baked_cookies - sales_cookies
 
-# 2. Ask for cookies per day and estimate per month
+full_box_label = "box" if full_box == 1 else "boxes"
+remain_cookies_label = "cookie" if remain_cookies == 1 else "cookies"
+
+print(f"You sell {full_box} {full_box_label} and {remain_cookies} {remain_cookies_label}. The leftover is {left_over} {remain_cookies_label} every day.")
+
+
 # 3. Ask for a number and check if it's even or odd
 # 4. Ask for meal cost and calculate tax + tip
 # 5. Show results with rounding and formatting
