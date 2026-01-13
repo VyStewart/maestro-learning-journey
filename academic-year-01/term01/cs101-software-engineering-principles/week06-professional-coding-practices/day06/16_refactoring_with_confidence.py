6aff153c51cfc8792c71a779cbfc4edb72a14eb4
VyStewart <vyt.stewart@gmail.com>
2026-01-12 19:22:54 -0600

    Week06 - day06 - Lesson: Refactoring with confidence

--- academic-year-01/term01/cs101-software-engineering-principles/week06-professional-coding-practices/day06/16_refactoring_with_confidence.py
+++ academic-year-01/term01/cs101-software-engineering-principles/week06-professional-coding-practices/day06/16_refactoring_with_confidence.py
@@ -2,3 +2,25 @@
 ### Software Engineering Principles
 ### Lesson 16: Refactoring with confidence
 ### Date: Jan 11 2026
+
+# Before
+def total_price(p,q,t):
+    subtotal = p * q
+    tax_amount = subtotal * t
+    final = subtotal + tax_amount
+    print("Final price:", final)
+
+total_price(10, 3, 0.07)
+
+# After 
+def calculate_tax(subtotal, tax_rate):
+    tax_amount = subtotal * tax_rate
+    return tax_amount
+
+def calculate_total_price(p,q,t):
+    subtotal = p * q
+    tax_amount = calculate_tax(subtotal, t)
+    final = subtotal + tax_amount
+    print("Final price:", final)
+
+calculate_total_price(10, 3, 0.07)
