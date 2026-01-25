ca49c5dc5dc5881fd10d39ad96f5394b078a0f8a
VyStewart <vyt.stewart@gmail.com>
2026-01-24 21:28:33 -0600

    Week 08 : Breaking down the problems
    Lesson 05: Breaking down into subproblem
    Lesson 06: Defining subproblem input s and outputs

---
+++ academic-year-01/term01/cs101-software-engineering-principles/week08-breaking-problems-down/day01/05_breaking_down_into_subproblem.py
@@ -0,0 +1,44 @@
+### CS101:  Software Engineering Principles
+### Week 08: Breaking Down the probles
+## Lesson 05: Breaking down into subproblem
+### Date: Jan 22 - 2026
+
+# Example:
+
+# 1. Set up the data for today's step total
+total_steps = 0
+print("Starting step counter for today")
+
+# 2. Let user log how many steps they just walked
+#    2.1 Ask user how many steps they walked
+new_steps = int(input("How many steps did you walk? "))
+print(" New Steps counted:", new_steps)
+
+#    2.2 Add that amount to today's total
+total_steps += new_steps
+
+# 3. Show today's total steps
+print("Total steps walking:", total_steps)
+
+# 4. If total is at least 10_000, show a "goal reached" message
+
+# Practice
+
+# 1. Set up today's total study minutes
+total_minutes = 0
+print("Starting study time:")
+
+# 2. Let the user enter how many minutes for this study session
+#    2.1 Ask how many minutes this session
+minutes = int(input("How many minutes did you study per session? "))
+print("Study time:", minutes)
+
+#    2.2 Add the minutes to today's total minutes
+total_minutes += minutes
+
+# 3. Show today's total minutes
+print("Total minutes:", total_minutes)
+
+# 4. Show a message when the total minutes reach at least 120
+if total_minutes >= 120:
+    print(" Great job! You reached your 120-minutesb goal")
---
+++ academic-year-01/term01/cs101-software-engineering-principles/week08-breaking-problems-down/day01/06_defining_subproblems_inputs_and_outputs.py
@@ -0,0 +1,38 @@
+### CS101:  Software Engineering Principles
+### Week 08: Breaking Down the probles
+## Lesson 06: Defining subproblems inputs and outputs
+### Date: Jan 22 - 2026
+
+# Example:
+def minutes_to_hours(total_minutes):
+    hours = total_minutes / 60
+    return hours
+
+result = minutes_to_hours(150)
+print(f"Study time: {result} hours")
+
+# Practice:
+def classify_score(score):
+    if score < 0:
+        return "error"
+    elif score >= 75:
+        return "pass"
+    else:
+        return "fail"
+
+result = classify_score(55)
+print(result)
+
+# Practice:
+def celsius_to_fahrenheit(celsius):
+    if not isinstance(celsius, (int, float)):
+        return "Invalid input"
+
+    fahrenheit = celsius * 9/5 + 32
+    return fahrenheit
+
+result = celsius_to_fahrenheit("twenty")
+print(result)
+
+
+
