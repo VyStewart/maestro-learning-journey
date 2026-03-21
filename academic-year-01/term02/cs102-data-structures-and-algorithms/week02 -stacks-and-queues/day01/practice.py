8e41dbf5bff4ae5d8ba5be9c7043e752a59957c1
VyStewart <155506936+VyStewart@users.noreply.github.com>
2026-03-21 17:01:02 -0500

    Building a stack using lists

---
+++ academic-year-01/term02/cs102-data-structures-and-algorithms/week02 -stacks-and-queues/day01/practice.py
@@ -0,0 +1,32 @@
+### CS102 - Data structures & algorithms 
+### Week 02 - Stacks and queues
+### Date: March 21 - 2026
+
+# Stack operations: push and pop
+books = []
+books.append("The Magic of Believing")
+print("\n",books)
+
+books.append("The Book of Enoch")
+print("\n", books)
+
+books.append("You Are The Way")
+print("\n",books)
+
+removed_book = books.pop()
+print(f"\nThe removed book is: {removed_book}")
+print(f'\nThe collection of books is: {books}')
+
+# Building a stcak using lists
+stack = []
+stack.append("home")
+stack.append("about")
+stack.append("contact")
+
+while stack:
+    page = stack.pop()
+    print("\nPopped:", page)
+
+print("\nFinal stack:", stack)
+
+
